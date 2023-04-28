0247ea460f84d8701e1b723929ade5916af9dad8
am999 <am9999072080@gmail.com>
2023-04-29 02:10:00 +0300

    Commit-1

--- src/main/java/pro/calculator/calculatorskypro/CalculatorService.java
+++ src/main/java/pro/calculator/calculatorskypro/CalculatorService.java
@@ -1,3 +1,13 @@
 package pro.calculator.calculatorskypro;
 
+public interface CalculatorService {
+    String answerCalculator();
 
+    String answerSum();
+
+    String answerMinus();
+
+    String answerMultiply();
+
+    String answerDivide();
+}
